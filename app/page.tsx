0b3e85ec5bc9a0571fa28a784caea3de77c44fae
Benavides, Gianni Stiven (Alliance Bioversity-CIAT) <g.benavides@cgiar.org>
2026-01-22 15:17:41 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -25,9 +25,7 @@ export default function Home() {
               Templates
             </a>{" "}
             or the{" "}
-            <a
-
-            &%%&/
+            <a        
               href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               className="font-medium text-zinc-950 dark:text-zinc-50"
             >
